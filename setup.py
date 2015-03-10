504179523be4828dcbf9711926d450a5f3b1b75e
Egil Moeller <egil.moller@freecode.no>
2015-03-10 18:02:24 +0100

    Clickification

--- setup.py
+++ setup.py
@@ -23,7 +23,10 @@ setup(
     description = "kmltrack converts a track (a series of lat/lon/timestamp and optional measurement values) into a kml file.",
     keywords = "kml",
     install_requires = ["python-dateutil"],
-    version = "0.0.2",
+    extras_require = {
+        'cli':  ["click"]
+    },
+    version = "0.0.3",
     author = "Egil Moeller",
     author_email = "egil@skytruth.org",
     license = "GPL",
